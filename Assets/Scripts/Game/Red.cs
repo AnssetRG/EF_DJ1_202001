9f901a3f3fb32292c4c316493eba722140983acf
JManAn <32886493+JManAn@users.noreply.github.com>
2020-07-11 10:52:38 -0500

    Commit Poderoso

--- Assets/Scripts/Game/Red.cs
+++ Assets/Scripts/Game/Red.cs
@@ -6,7 +6,7 @@ public class Red : EnemyController
 {
     protected override void Start()
     {
-        SetUp(-0.9f, false);
+        SetUp(-0.75f, false);
         points = 10;
         this.tag = "Fly";
     }
--- Assets/Scripts/Game/Yellow.cs
+++ Assets/Scripts/Game/Yellow.cs
@@ -6,7 +6,7 @@ public class Yellow : EnemyController
 {
     protected override void Start()
     {
-        SetUp(-0.9f, false);
+        SetUp(-0.75f, false);
         points = 15;
         this.tag = "Yellow";
     }
